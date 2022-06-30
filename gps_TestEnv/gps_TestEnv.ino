005c4431b02fa085c56e6f2b204d0a00b002328a
Wojciech <sapon.wojciech@outlook.com>
2022-06-30 20:17:06 +0200

    added Wi-Fi connection

--- gps_TestEnv/gps_TestEnv.ino
+++ gps_TestEnv/gps_TestEnv.ino
@@ -1,9 +1,33 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+\\Wi-Fi information
+#define ssid "ESPtest"
+#define password "MQTTtest2137!"
+
+\\contstructor
+WiFiClient wifiConnection;
+PubSubClient client(wifiConnection;);
+
+\\connect to wi-fi method
+
+void wifi_connect(){ 
+  WiFi.begin(ssid, password);             
+  while (WiFi.status() != WL_CONNECTED) { 
+    delay(500);
+  }
+  Serial.println("Connected to network with ip:");  
+  Serial.println(WiFi.localIP());        
+}
+
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  wifi_connect();
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ 
 
 }
