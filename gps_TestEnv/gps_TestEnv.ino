456f0e915b0eded02ed7b07a2e3be30d8a0311b9
Wojciech <sapon.wojciech@outlook.com>
2022-07-06 20:32:39 +0200

    added GPS functionality

--- gps_TestEnv/gps_TestEnv.ino
+++ gps_TestEnv/gps_TestEnv.ino
@@ -1,9 +1,16 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+#include <Ticker.H>
+
 
 // Wi-Fi information
 #define ssid "ESPtest"
 #define password "MQTTtest2137!"
+#define rxPin 4
+#define txPin 5
+#define baudRate 115200
 
 // MQTT setup
 char *mqtt_broker = "broker.emqx.io";
@@ -12,17 +19,26 @@ char *mqtt_uname= "test";
 char *mqtt_passwd= "test";
 char *sendTopic= "esp/sendtopic";
 char *reciveTopic= "esp/recivetopic";
+char *gpsstatus= "esp/gpsstatus";
+
+
+Ticker gpssend;
+
 
 // constructors
 WiFiClient wifiConnection;
 PubSubClient client(wifiConnection);
+TinyGPSPlus gps;
+SoftwareSerial gpsSerial(rxPin, txPin);
 
 
 // connect to wi-fi method
 void wifi_connect(){ 
   WiFi.begin(ssid, password);             
+  serial.print("connecting to WI-FI");
   while (WiFi.status() != WL_CONNECTED) { 
     delay(500);
+    serial.print(".").
   }
   Serial.println("Connected to network with ip:");  
   Serial.println(WiFi.localIP());        
@@ -45,24 +61,49 @@ void mqtt_connect(){
 }
 
 void callback(char *topic, byte *payload, unsigned int length) {
-  Serial.print("Message arrived in topic: ");
+  Serial.print("new messeage in topic: ");
   Serial.println(reciveTopic);
   Serial.print("Message:");
   for (int i = 0; i < length; i++) {
       Serial.print((char) payload[i]);
   }
-  Serial.println();
-  Serial.println("-----------------------");
 }
 
+void checkGPS()
+{
+  if (gps.charsProcessed() < 10){
+    client.publish(gpsstatus, "GPSError");
+  }
+  else{
+    client.publish(gpsstatus,"GPSOK")
+  }
+}
+
+void sendGPSData(){
+   while (SerialGPS.available() >0) {
+       gps.encode(SerialGPS.read());
+    }
+    client.publish(sats,gps.satellites.value());
+    client.publish(lat,gps.location.lat()());
+    client.publish(lng,gps.location.lng();());
+    client.publish(day,gps.date.day());
+    client.publish(month,gps.date.month());
+    client.publish(year,gps.date.year());
+    client.publish(hour,gps.time.hour());
+    client.publish(minute,gps.time.minute());
+    client.publish(second,gps.time.second());
+
+}
+   
 
 void setup() {
 
-  Serial.begin(115200);
+  Serial.begin(baudRate);
+  gpsSerial.begin(baudRate)
   client.setCallback(callback);
   wifi_connect();
   mqtt_connect();
-  
+  gpssend.attach(0.5,sendGPSData);
 }
 
 void loop() {
