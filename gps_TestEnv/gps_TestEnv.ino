bd02a9aa47baa0026435acef3625253225850860
Wojciech <sapon.wojciech@outlook.com>
2022-07-12 22:03:58 +0200

    correction in function

--- gps_TestEnv/gps_TestEnv.ino
+++ gps_TestEnv/gps_TestEnv.ino
@@ -90,53 +90,65 @@ String lat_str, lng_str, spd_str,
 char lat_ch[20] , lng_ch[20], spd_ch[20], 
           date_ch[20], time_ch[20], sat_ch[20], crs_ch[20];  
 
+void sendGpsData(){
 
-void loop()
-{
- client.loop();
  
+          
+          latitude = gps.location.lat();
+          lat_str = String(latitude , 6); 
+          lat_str.toCharArray(lat_ch, lat_str.length() + 1);
+          
+          longitude = gps.location.lng();
+          lng_str = String(longitude , 6); 
+          lng_str.toCharArray(lng_ch, lng_str.length() + 1);      
+         
+          speed = gps.speed.kmph();
+          spd_str = String(speed); 
+          spd_str.toCharArray(spd_ch, spd_str.length() + 1);      
+         
+          date = gps.date.value();
+          date_str = String(date,0); 
+          date_str.toCharArray(date_ch, date_str.length() + 1);      
+         
+          _time = gps.time.value();
+          time_str = String(_time,0); 
+          time_str.toCharArray(time_ch, time_str.length() + 1);      
+        
+          satellites =  gps.satellites.value();
+          sat_str = String(satellites); 
+          sat_str.toCharArray(sat_ch, sat_str.length() + 1); 
+        
+          course = gps.course.deg();
+          crs_str = String(course,0); 
+          crs_str.toCharArray(crs_ch, crs_str.length() + 1); 
+          
+          client.publish(lngTopic, lng_ch);
+          client.publish(latTopic, lat_ch);
+          client.publish(spdTopic, spd_ch);
+          client.publish(crsTopic, crs_ch);
+          client.publish(dateTopic, date_ch);
+          client.publish(timeTopic, time_ch);
+          client.publish(satTopic, sat_ch);
+          }
+
+
+unsigned long previousMillis = 0;
+const long interval = 2000;   
+
+void loop(){
+  unsigned long currentMillis = millis();
+ client.loop();
   while (gpsSerial.available() > 0) {
     if (gps.encode(gpsSerial.read()))
       {
         if (gps.location.isValid())
         {
-          Serial.print( gps.course.deg());
-          latitude = gps.location.lat();
-          lat_str = String(latitude , 6); 
-          lat_str.toCharArray(lat_ch, lat_str.length() + 1);
-          client.publish(latTopic, lat_ch);
-         
-          longitude = gps.location.lng();
-          lng_str = String(longitude , 6); 
-          lng_str.toCharArray(lng_ch, lng_str.length() + 1);      
-          client.publish(lngTopic, lng_ch);
-         
-          speed = gps.speed.kmph();
-          spd_str = String(speed); 
-          spd_str.toCharArray(spd_ch, spd_str.length() + 1);      
-          client.publish(spdTopic, spd_ch);
-         
-          date = gps.date.value();
-          date_str = String(date,0); 
-          date_str.toCharArray(date_ch, date_str.length() + 1);      
-          client.publish(dateTopic, date_ch);
-          
-          _time = gps.time.value();
-          time_str = String(_time,0); 
-          time_str.toCharArray(time_ch, time_str.length() + 1);      
-          client.publish(timeTopic, time_ch);
-         
-          satellites =  gps.satellites.value();
-          sat_str = String(satellites); 
-          sat_str.toCharArray(sat_ch, sat_str.length() + 1); 
-          client.publish(satTopic, sat_ch);
-         
-          course = gps.course.deg();
-          crs_str = String(course,0); 
-          crs_str.toCharArray(crs_ch, crs_str.length() + 1); 
-          client.publish(crsTopic, crs_ch);
-               delay (3000);
-          }
-       }
+ if (currentMillis - previousMillis >= interval) {
+ previousMillis = currentMillis;
+ sendGpsData();
+ Serial.print("pwi");
+        }
     }
+}
+}
 }
