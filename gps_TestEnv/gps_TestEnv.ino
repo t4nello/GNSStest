7d23ba6777fa55b754abd5e65ea34de8982cc99d
t4nello <81033764+t4nello@users.noreply.github.com>
2023-09-27 18:23:39 +0200

    Update gps_TestEnv.ino

--- gps_TestEnv/gps_TestEnv.ino
+++ gps_TestEnv/gps_TestEnv.ino
@@ -3,6 +3,7 @@
 #include <MQTT.h>
 #include <TinyGPSPlus.h>
 #include <SoftwareSerial.h>
+#include <ArduinoJson.h>
 
 #define RxPin 4
 #define txPin 5
@@ -21,6 +22,7 @@ WiFiClient net;
 MQTTClient client;
 
 unsigned long lastMillis = 0;
+bool enableGps = false;
 
 void connect() {
   Serial.print("checking wifi...");
@@ -39,44 +41,84 @@ void connect() {
   client.subscribe("gps/metric/disable");
 }
 
-bool enableGps = true;
+int sessionID = 0 ;
 void messageReceived(String &topic, String &payload ) {
-
-if (topic.compareTo("esp/detect") && payload.compareTo(WiFi.macAddress()) == 0){
+  if (topic.compareTo("esp/detect") == 0 && payload.compareTo(WiFi.macAddress()) == 0) {
     Serial.println(F("lokalizuj"));
-} else if (topic.compareTo("gps/metric/enable") == 0){
-  enableGps = true;
-  Serial.println(F("wllacz"));
-} else if (topic.compareTo("gps/metric/disable") == 0 ){
-  enableGps = false; 
-  Serial.println(F("wylacz"));
+   digitalWrite(LED_BUILTIN, HIGH);
+  } else if (topic.compareTo("gps/metric/enable") == 0) {
+    enableGps = true;
+    Serial.println(F("wllacz"));
+  } else if (topic.compareTo("gps/metric/disable") == 0) {
+    enableGps = false; 
+    Serial.println(F("wylacz"));
+  }
 }
+String convertGPSDateTime(String date, String time) {
+
+  int year = date.substring(4, 6).toInt();
+  int month = date.substring(2, 4).toInt();
+  int day = date.substring(0, 2).toInt();
+ date =  String(month < 10 ? "0" : "") + String(month) + "/" +
+         String(day < 10 ? "0" : "") + String(day) + "/20" +
+         String(year < 10 ? "0" : "") + String(year);
+
+  int hour = time.substring(0, 2).toInt();
+  int minute = time.substring(2, 4).toInt();
+  int second = time.substring(4, 6).toInt();
+   time = String(hour < 10 ? "0" : "") + String(hour) + ":" +
+         String(minute < 10 ? "0" : "") + String(minute) + ":" +
+         String(second < 10 ? "0" : "") + String(second);
+  return date + " " + time;
+}
+
+
+void publishInfo()
+{
+String dateString = String(gps.date.value());
+String timeString = String(gps.time.value());
+Serial.print(timeString);
+String datetime = convertGPSDateTime(dateString, timeString);
+DynamicJsonDocument doc(1024);
+doc["Device"] = String(WiFi.macAddress());
+doc["Latitude"] = gps.location.lat();
+doc["Longitude"] = gps.location.lng();
+doc["Date"] = datetime;
+String jsonStr;
+  serializeJson(doc, jsonStr);
+   Serial.print('\n');
+
+client.publish("gps/metric", jsonStr);
+serializeJson(doc, Serial);
 
 }
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
   ss.begin(GPSBaud);
   Serial.begin(baudRate);
   WiFi.begin(ssid, pass);
   client.begin("192.168.0.123", net);
-  client.onMessage(messageReceived);
-
   connect();
 }
 
 void loop() {
-  client.loop();
 
+  client.loop();
+   
+  client.onMessage(messageReceived);
   if (!client.connected()) {
     connect();
   }
+ 
   while (ss.available() > 0)
     if (gps.encode(ss.read()))
-      
     if (gps.location.isValid() && enableGps == true ){
-      if (millis() - lastMillis > 5000) {
-          client.publish("gps/" + WiFi.macAddress() + "/Latitude", String(gps.location.lat(),6));
+      if (millis() - lastMillis > 3000) {
+          publishInfo();
           lastMillis = millis();
     }
   }
 }
+
